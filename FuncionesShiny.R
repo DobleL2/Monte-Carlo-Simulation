b676170158af0a6ec3ac38c8776a7bac21e0510f
Mathew Cisneros <mathewcisneros@MacBook-Air-de-Mathew.local>
2024-06-18 21:41:33 -0500

    Velas japonesas concluidas

--- FuncionesShiny.R
+++ FuncionesShiny.R
@@ -19,7 +19,7 @@ ncoldata<- puntos_dias + 5
 
 simular_precios_diarios <- function(Po) {
   precios_aleatorios <- Po + cumsum(rnorm(puntos_dias, mean = 0, sd = 2))  # Generar variaciones de precios
-  c(Po, precios_aleatorios, max(precios_aleatorios), min(precios_aleatorios), tail(precios_aleatorios, 1))
+  c(Po, precios_aleatorios, max(c(precios_aleatorios,Po)), min(c(precios_aleatorios,Po)), tail(precios_aleatorios, 1))
 }
 
 t<- simular_precios_diarios(10)
@@ -112,18 +112,18 @@ library(plotly)
 # Preparar datos para el gráfico de velas
 datos_velas <- data.frame(
   Dia = resultados$Dia,
-  Open = resultados$P0,
-  Close = resultados$Po_siguiente,
-  High = resultados$Max,
-  Low = resultados$Min
+  Apertura = resultados$P0,
+  Cierre = resultados$Po_siguiente,
+  Maximo = resultados$Max,
+  Minimo = resultados$Min
 )
 
 datos_velas
 
 # Crear gráfico de velas japonesas
 fig <- plot_ly(datos_velas, x = ~Dia, type = "candlestick",
-               open = ~Open, close = ~Close,
-               high = ~High, low = ~Low) %>%
+               open = ~Apertura, close = ~Cierre,
+               high = ~Maximo, low = ~Minimo) %>%
   layout(title = "Gráfico de Velas Japonesas de Precios Diarios",
          xaxis = list(title = "Día"),
          yaxis = list(title = "Precio"),
